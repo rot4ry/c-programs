f1548bbe72d1ebfd0b7d51532da583cfc30b702d
rot4ry <45862201+rot4ry@users.noreply.github.com>
2018-12-14 10:50:19 +0100

    Add files via upload

---
+++ vaporwave.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* *argv)
+{
+    system("clear");
+    printf("\t\v\v\t");
+    for(int i=0; i<argc; i++)
+    {
+        for(int j=0; j<=strlen(argv[i]); j++)
+        {
+            if(i==0)
+            {
+                printf("%c ", argv[i][j]);    
+            }
+
+            else
+            printf("%c  ", argv[i][j]);
+        }
+
+        if(i==0)
+        {
+            printf("\n\n\t\t\t");
+        }
+        else 
+        printf("    ");
+    }
+    getchar();
+    return 0;
+}
